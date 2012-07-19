d2d739b8c1e4de6038955e5a7040533393b68a3c
Jim Purbrick <jimpurbrick@gmail.com>
2012-07-19 18:24:08 +0000

    Added oauth login support.

--- crestexplorer.js
+++ crestexplorer.js
@@ -17,12 +17,16 @@
 /*jslint undef: true, browser: true, vars: true, white: true, forin: true, plusplus: true, bitwise: true, eqeq: true, maxerr: 50, indent: 4 */
 /*global $ */
 
-(function () { // Start crestexplorerjs
+(function ($, window, document) { // Start crestexplorerjs
 
     "use strict";
 
-    // force cross-site scripting (needed for IE9 CORS support with jQuery 1.5)
-    jQuery.support.cors = true;
+    // Configuration parameters
+    var redirectUri = "http://10.1.4.51:8888/index.html";
+    var clientId = "localcrestexplorerjs"; // OAuth client id
+    var csrfTokenName = clientId + "csrftoken";
+    var hashTokenName = clientId + "hash";
+    var scopes = "capsuleerRead personalContactsRead corporationContactsRead";
     
     function bind() {
         $("a").click(function(evt) {
@@ -41,7 +45,24 @@
     }
 
     function isLink(value) {
-        return isObject(value) && (value.href !== undefined);
+        var prop;
+        if (! isObject(value)) {
+            return false;
+        }
+        if (value.href === undefined) {
+            return false;
+        }
+        for (prop in value) {
+            if (value.hasOwnProperty(prop)) {
+                if (isArray(value[prop])) {
+                    return false;
+                }
+                if (isObject(value[prop])) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     function buildElementFromPrimitive(data) {
@@ -52,10 +73,10 @@
         var link = $(document.createElement('a'))
             .attr('href', data.href)
             .addClass('name');
-        if(name !== undefined) {
-            $(link).append(name);
-        } else if(data.name != undefined) {
+        if(data.name !== undefined) {
             $(link).append(data.name);
+        } else if(name !== undefined) {
+            $(link).append(name);
         } else {
             $(link).append(data.href);
         }
@@ -82,14 +103,15 @@
 
     function buildElementFromObject(data) {
         var prop, list = document.createElement('ul');
+
+        if (data.href !== undefined) {
+            $(list).prepend(
+                $(document.createElement('li'))
+                    .addClass('dictionaryItem')
+                    .append(buildElementFromLink(data, data.name)));
+        }
         for (prop in data) {
-        if (data.hasOwnProperty(prop) ) {
-            if (isLink(data[prop])) {
-                $(list).prepend(
-                    $(document.createElement('li'))
-                        .addClass('dictionaryItem')
-                    .append(buildElementFromLink(data[prop], prop)));
-                } else {
+            if (data.hasOwnProperty(prop)) {
                 $(list).prepend(
                     $(document.createElement('li'))
                         .addClass('dictionaryItem')
@@ -99,7 +121,6 @@
                     .append($(document.createElement('span'))
                         .addClass('value')
                         .append(buildElement(data[prop]))));
-                }
             }
         }
         return $(list);
@@ -115,8 +136,16 @@
         return buildElementFromPrimitive(data);
     }
 
-    function render(path) {
-        $.getJSON(path, function(data, status, xhr) {
+    function displayError(error) {
+        $("#data").children().replaceWith("<span>" + error + "</span>");
+    }
+
+    function render(uri) {
+        if (uri.indexOf("http") !== 0) {
+            displayError("Addresses must be absolute");
+            return;
+        }
+        $.getJSON(uri, function(data, status, xhr) {
             $("#data").children().replaceWith(buildElement(data));
             bind();
         });
@@ -135,19 +164,91 @@
         }
     }
 
-    $(document).ready(function() {
+    function onClickLogin(evt) {
+        var command = $("#login").text();
+        if (command === "login") {
+
+            // Store CSRF token and current location as cookie
+            var csrfToken = uuidGen();
+            $.cookie(csrfTokenName, csrfToken);
+            $.cookie(hashTokenName, window.location.hash);
+
+            // No OAuth token, request one from the OAuth authentication endpoint
+            window.location =  "http://login.jim01.dev/oauth/Authorize/" +
+                "?response_type=token" +
+                "&client_id=" + clientId +
+                "&scope=" + scopes +
+                "&redirect_uri=" + redirectUri +
+                "&state=" + csrfToken;
+
+        } else {
+            ajaxSetup(false);
+            loginSetup(false);
+        }
+        evt.preventDefault();
+    }
+
+    // Extract value from oauth formatted hash fragment.
+    function extractFromHash(name, hash) {
+        var match = hash.match(new RegExp(name + "=([^&]+)"));
+        return !!match && match[1];
+    }
+
+    // Generate an RFC4122 version 4 UUID
+    function uuidGen() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+    }
+
+    function ajaxSetup(token) {
+        var headers = {
+            "Accept": "application/json, charset=utf-8"
+        };
+        if (token) {
+            headers.Authorization = "Bearer " + token;
+        }
         $.ajaxSetup({
             accepts: "application/json, charset=utf-8",
             crossDomain: true,
             type: "GET",
             dataType: "json",
-            headers: {
-                "Accept": "application/json, charset=utf-8"
-            },
+            headers: headers,
             error: function (xhr, status, error) {
-                $("#data").children().replaceWith("<span>" + error + "</span>");
+                displayError(error);
             }
         });
+    }
+
+    function loginSetup(token) {
+        $("#login").text(token? "logout":"login").click(onClickLogin);
+    }
+
+    $(document).ready(function() {
+
+        var hash = window.location.hash;
+        var token = extractFromHash("access_token", hash);
+
+        if (token) {
+
+            // Check CSRF token in state matches token saved in cookie
+            if(extractFromHash("state", hash) !== $.cookie(csrfTokenName)) {
+                displayError("CSRF token mismatch");
+                return;
+            }
+
+            // Restore hash.
+            window.location.hash = $.cookie(hashTokenName);
+
+            // Delete cookies.
+            $.cookie(csrfTokenName, null);
+            $.cookie(hashTokenName, null);
+        }
+
+        ajaxSetup(token);
+        loginSetup(token);
+
         $("#autorefresh > input").click(onClickAutoRefresh);
         refresh();
     });
@@ -156,4 +257,4 @@
         refresh();
     };
 
-}()); // End crestexplorerjs
+}($, window, document)); // End crestexplorerjs
